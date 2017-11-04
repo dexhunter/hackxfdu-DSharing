d49c88571462cb470ff6c6936431a914d05fd86a
PRIEWIENV <styeah@live.com>
2017-11-04 20:49:40 +0800

    add static files and templates

---
+++ src/server/static/js/app.js
@@ -0,0 +1,94 @@
+// $(function() {
+//     $(window).scroll(function() {
+//         var winTop = $(window).scrollTop();
+//         if (winTop >= 30) {
+//             $("body").addClass("sticky-shrinknav-wrapper");
+//         } else{
+//             $("body").removeClass("sticky-shrinknav-wrapper");
+//         }
+//     });
+// });
+
+$(document).foundation();
+
+$('.title-bar').on('sticky.zf.stuckto:top', function(){
+    $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top', function(){
+    $(this).removeClass('shrink');
+})
+
+$(document).ready(function () {
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
+    Highcharts.chart('container', {
+        chart: {
+            type: 'spline',
+            animation: Highcharts.svg, // don't animate in old IE
+            marginRight: 10,
+            events: {
+                load: function () {
+
+                    // set up the updating of the chart each second
+                    var series = this.series[0];
+                    setInterval(function () {
+                        var x = (new Date()).getTime(), // current time
+                            y = {{height}};
+                        series.addPoint([x, y], true, true);
+                    }, 1000);
+                }
+            }
+        },
+        title: {
+            text: 'Blockchain Height'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150
+        },
+        yAxis: {
+            title: {
+                text: 'Value'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.series.name + '</b><br/>' +
+                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                    Highcharts.numberFormat(this.y, 2);
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        exporting: {
+            enabled: false
+        },
+        series: [{
+            name: 'Random data',
+            data: (function () {
+                // generate an array of random data
+                var data = [],
+                    time = (new Date()).getTime(),
+                    i;
+
+                for (i = -19; i <= 0; i += 1) {
+                    data.push({
+                        x: time + i * 1000,
+                        y: {{height}}
+                });
+                }
+                return data;
+            }())
+        }]
+    });
+});
+
